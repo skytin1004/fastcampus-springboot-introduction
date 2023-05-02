9ffd44fde982c2e5050580f4f63d99387e9676db
minseok <99078115+skytin1004@users.noreply.github.com>
2023-05-02 12:21:30 +0900

    study0502

--- 06. 스프링의 기능을 활용해보자/SourceCode/exception/src/main/java/com/example/exception/advice/ApiControllerAdvice.java
+++ 06. 스프링의 기능을 활용해보자/SourceCode/exception/src/main/java/com/example/exception/advice/ApiControllerAdvice.java
@@ -21,22 +21,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+// 예외처리할 때 사용하는 어노테이션 @RestControllerAdvice , @ControllerAdvice
+// 둘 다 예외처리할 때 사용하는 어노테이션이지만 RestControllerAdvice는 예외처리에 대한 응답으로 객체를 리턴하는 기능이 추가로 있다. -> ResponseEntity를 이용해서 객체 리턴
+@RestControllerAdvice(basePackageClasses = ApiController.class) //ApiController에서 발생하는 예외를 처리한다는 뜻.
+public class ApiControllerAdvice {   
 
-@RestControllerAdvice(basePackageClasses = ApiController.class)
-public class ApiControllerAdvice {
+    //ExceptionHandler : 예외가 발생했을 때 ExceptionHandler에서 지정한 방식으로 예외를 처리하게 해주는 기능.(메소드에서 지정한 로직으로 예외 처리)
+    @ExceptionHandler(value = Exception.class) // Exception클래스의 예외가 발생했을 때 해당 메소드 작동 (모든 자바의 예외 클래스는 Exception.class의 상속을 받는다.)
+    public ResponseEntity exception(Exception e){ 
+        //getClass().getName : 패키지 이름이 포함된 클래스 이름. getClass().getSimpleName(): 패키지 경로가 포함되지 않은 클래스이름
+        System.out.println(e.getClass().getName()); //오류의 클래스와 이름을 출력한다.
 
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity exception(Exception e){
-        System.out.println(e.getClass().getName());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+        //서버 에러를 표시한다.
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(""); //반환값에 상태코드와 응답 메세지를 주고 싶을 때 사용되는ResponseEntity
     }
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    // methodArgumentNotValidException: Json 파싱이 제대로 이루어진 후 @Valid 단계에서 오류가 있는 경우 발생
+    @ExceptionHandler(value = MethodArgumentNotValidException.class) //MethodArgumentNotValidException.class의 예외가 발생했을 때 해당 메소드 작동
     public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest httpServletRequest){
 
-        List<Error> errorList = new ArrayList<>();
+        List<Error> errorList = new ArrayList<>(); //새로운 리스트 생성.
 
-        BindingResult bindingResult = e.getBindingResult();
+        BindingResult bindingResult = e.getBindingResult(); // 여기부터
         bindingResult.getAllErrors().forEach(error -> {
             FieldError field = (FieldError) error;
 
