bfad1fc7b1af87dcb3b1fc4f87ba29a55c6fd86d
minseok <99078115+skytin1004@users.noreply.github.com>
2023-05-02 09:51:10 +0900

    studt0502

--- 06. 스프링의 기능을 활용해보자/SourceCode/exception/src/main/java/com/example/exception/dto/Error.java
+++ 06. 스프링의 기능을 활용해보자/SourceCode/exception/src/main/java/com/example/exception/dto/Error.java
@@ -6,6 +6,8 @@ public class Error {
     private String message;
     private String invalidValue;
 
+    //각 매개변수의 getter,setter 생성 - 롬복을 이용해서 간단히 생성할 수 도 있다.
+
     public String getField() {
         return field;
     }
