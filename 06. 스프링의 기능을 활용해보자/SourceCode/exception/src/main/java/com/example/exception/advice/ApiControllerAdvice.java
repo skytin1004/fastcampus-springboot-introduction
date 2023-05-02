ce199e332bd1e22bb64a01db94596adec12c5552
minseok <99078115+skytin1004@users.noreply.github.com>
2023-05-02 13:24:23 +0900

    0502

--- 06. 스프링의 기능을 활용해보자/SourceCode/exception/src/main/java/com/example/exception/advice/ApiControllerAdvice.java
+++ 06. 스프링의 기능을 활용해보자/SourceCode/exception/src/main/java/com/example/exception/advice/ApiControllerAdvice.java
@@ -30,7 +30,7 @@ public class ApiControllerAdvice {
     @ExceptionHandler(value = Exception.class) // Exception클래스의 예외가 발생했을 때 해당 메소드 작동 (모든 자바의 예외 클래스는 Exception.class의 상속을 받는다.)
     public ResponseEntity exception(Exception e){ 
         //getClass().getName : 패키지 이름이 포함된 클래스 이름. getClass().getSimpleName(): 패키지 경로가 포함되지 않은 클래스이름
-        System.out.println(e.getClass().getName()); //오류의 클래스와 이름을 출력한다.
+        System.out.println(e.getClass().getName()); //오류의 패키지 이름이 포함된 클래스 이름을 출력한다.
 
         //서버 에러를 표시한다.
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(""); //반환값에 상태코드와 응답 메세지를 주고 싶을 때 사용되는ResponseEntity
@@ -41,8 +41,8 @@ public class ApiControllerAdvice {
 
         List<Error> errorList = new ArrayList<>(); //새로운 리스트 생성.
 
-        BindingResult bindingResult = e.getBindingResult(); // 여기부터
-        bindingResult.getAllErrors().forEach(error -> {
+        BindingResult bindingResult = e.getBindingResult(); // getBindingResult():해당 BindingResult에서 발생한 에러를 가져온다.
+        bindingResult.getAllErrors().forEach(error -> { //해당 BindingResult에서 발생한 에러를 모두 가져와 foreach를 통해 작업한다.
             FieldError field = (FieldError) error;
 
             String fieldName = field.getField();
